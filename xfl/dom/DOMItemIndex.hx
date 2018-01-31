c25c5d7d88be7948b9e57952bade6628180c0670
Andreas Drewke <andreas@drewke.net>
2018-01-31 20:44:59 +0100

    Memory optimizations: Forgot about this one :)

---
+++ xfl/dom/DOMItemIndex.hx
@@ -0,0 +1,15 @@
+package xfl.dom;
+
+import haxe.xml.Fast;
+
+class DOMItemIndex { 
+
+	public var name: String;
+	public var linkageClassName: String;
+	public var item: Dynamic;
+	public var fileName: String;
+
+	public function new () {
+	}
+
+}
